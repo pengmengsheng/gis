f0f5f20effd75882308606fbbe47b8e2c34567fb
Mr PM <718195578@qq.com>
2019-04-26 15:01:49 +0800

    Update jyzy.gis.arcgis.js

--- gis/js/gis/jyzy.gis.arcgis.js
+++ gis/js/gis/jyzy.gis.arcgis.js
@@ -1,31 +1,31 @@
 ;;;;
 var jyzy = window.jyzy || {};
 jyzy.gis = {}
-jyzy.gis.arcgis = {}
+gis.arcgis = {}
 
-jyzy.gis.arcgis.defaultHighlightOption = {
+gis.arcgis.defaultHighlightOption = {
 	    lineColor: [255, 0, 0],//边框颜色
 	    lineWidth: 3,//边框宽度
 	    fillColor: [125, 125, 125],//填充颜色
 	    fillAlhpaValue: 0.35//填充alpha通道颜色
 	};
-jyzy.gis.arcgis._selectable = false;
-jyzy.gis.arcgis._graphic = null;
-jyzy.gis.arcgis._highlightSymbol = null;
-jyzy.gis.arcgis._layerList = null;
-jyzy.gis.arcgis._map = null;
-jyzy.gis.arcgis._workLayer = null;
-jyzy.gis.arcgis._mapView = null;
-jyzy.gis.arcgis._selectedFeatures = [];
-jyzy.gis.arcgis._mapClickEvent = null;
-jyzy.gis.arcgis._clickEvent = null;
+gis.arcgis._selectable = false;
+gis.arcgis._graphic = null;
+gis.arcgis._highlightSymbol = null;
+gis.arcgis._layerList = null;
+gis.arcgis._map = null;
+gis.arcgis._workLayer = null;
+gis.arcgis._mapView = null;
+gis.arcgis._selectedFeatures = [];
+gis.arcgis._mapClickEvent = null;
+gis.arcgis._clickEvent = null;
 
-jyzy.gis.arcgis.draw=null;
+gis.arcgis.draw=null;
 
-jyzy.gis.arcgis.open = function(param,selectable = false){
-	jyzy.gis.arcgis.openMap(param);
+gis.arcgis.open = function(param,selectable = false){
+	gis.arcgis.openMap(param);
 	if(selectable){
-		jyzy.gis.arcgis.setSelectable(selectable);
+		gis.arcgis.setSelectable(selectable);
 	}
 }
 
@@ -40,12 +40,12 @@ jyzy.gis.arcgis.open = function(param,selectable = false){
  * highlightOption ： 高亮选中参数
  * showMenuContext:鼠标右键单击显示上下文菜单
  * */
-jyzy.gis.arcgis.openMap = function (param) {
-	var highlightOption = param.highlightOption ? param.highlightOption : jyzy.gis.arcgis.defaultHighlightOption;
+gis.arcgis.openMap = function (param) {
+	var highlightOption = param.highlightOption ? param.highlightOption : gis.arcgis.defaultHighlightOption;
 	
 	var defaultFeature = param.defaultFeature ? param.defaultFeature : 'building';
 	
-	jyzy.gis.arcgis._selectable = false;
+	gis.arcgis._selectable = false;
 	
 	require([
 	    'esri/views/MapView',
@@ -61,10 +61,10 @@ jyzy.gis.arcgis.openMap = function (param) {
 		"esri/views/2d/draw/Draw",
 	    "esri/geometry/geometryEngine"
 	], function (MapView, Map, MapImageLayer, FeatureLayer, LayerList, Graphic, Geometry, Extent,ScaleBar,PopupTemplate,Draw,geometryEngine){
-		jyzy.gis.arcgis._graphic = Graphic;
+		gis.arcgis._graphic = Graphic;
 		
 		highlightOption.fillColor.push(highlightOption.fillAlhpaValue);
-		jyzy.gis.arcgis._highlightSymbol = {
+		gis.arcgis._highlightSymbol = {
 		  type: 'simple-fill',  // autocasts as new SimpleFillSymbol()
 		  color: [ 51,51, 204, 0.4 ],
 		  style: 'solid',
@@ -73,8 +73,8 @@ jyzy.gis.arcgis.openMap = function (param) {
 		    width: 1
 		  }
 		};
-	    jyzy.gis.arcgis._layerList = param.layerLists;
-	    jyzy.gis.arcgis._map = new Map();
+	    gis.arcgis._layerList = param.layerLists;
+	    gis.arcgis._map = new Map();
 	    	
 	    let popupTemplate = {
 	    	//let popupTemplate = new PopupTemplate({
@@ -88,16 +88,16 @@ jyzy.gis.arcgis.openMap = function (param) {
 	    };
 	    let popupEnabled = false;
 	    
-	    jyzy.gis.arcgis.mapAddLayer(jyzy.gis.arcgis._layerList,param.baseUrl,FeatureLayer,MapImageLayer,popupTemplate,popupEnabled,defaultFeature);
+	    gis.arcgis.mapAddLayer(gis.arcgis._layerList,param.baseUrl,FeatureLayer,MapImageLayer,popupTemplate,popupEnabled,defaultFeature);
 	    
 	    let view = new MapView({
 	    	container: param.containerId,
-	        map: jyzy.gis.arcgis._map
+	        map: gis.arcgis._map
 	        //center
 	    });
 	    view.scale = 30000;
 	    
-	    jyzy.gis.arcgis._mapView = view;
+	    gis.arcgis._mapView = view;
 	    
 	    view.when(function() {
 	    	//show layer
@@ -135,16 +135,16 @@ jyzy.gis.arcgis.openMap = function (param) {
 	    });
 	    
 	    
-	    jyzy.gis.arcgis._workLayer.mapLayer.when(function () {
-	        view.whenLayerView(jyzy.gis.arcgis._workLayer.mapLayer).then(function (layerView) {
-	        	jyzy.gis.arcgis._workLayer.view = layerView;
+	    gis.arcgis._workLayer.mapLayer.when(function () {
+	        view.whenLayerView(gis.arcgis._workLayer.mapLayer).then(function (layerView) {
+	        	gis.arcgis._workLayer.view = layerView;
 	        });
 	    });
 	
 	    view.on('click', function (e) {
 	    	if(e.button != 0) return;
 	    	
-			jyzy.gis.arcgis._clickEventParam = e;
+			gis.arcgis._clickEventParam = e;
 			const geometry = new Extent({
 				xmax : e.mapPoint.x ,
 				ymax : e.mapPoint.y ,
@@ -152,7 +152,7 @@ jyzy.gis.arcgis.openMap = function (param) {
 				ymin : e.mapPoint.y
 			});
 			const append = e.native.ctrlKey;
-			jyzy.gis.arcgis.selectFeatureOnMap(geometry, append);
+			gis.arcgis.selectFeatureOnMap(geometry, append);
 	    });
 	
 	});
@@ -160,7 +160,7 @@ jyzy.gis.arcgis.openMap = function (param) {
 /**
  * map增加图层
  */
-jyzy.gis.arcgis.mapAddLayer = function(layers,baseUrl,FeatureLayer,MapImageLayer,popupTemplate,popupEnabled=false,defaultResourceName){
+gis.arcgis.mapAddLayer = function(layers,baseUrl,FeatureLayer,MapImageLayer,popupTemplate,popupEnabled=false,defaultResourceName){
 	for (let layer of layers) {
 		let category = layer['CATEGORY'].toLowerCase();
 		let resourceName = layer['FEATURE_ID'];
@@ -198,10 +198,10 @@ jyzy.gis.arcgis.mapAddLayer = function(layers,baseUrl,FeatureLayer,MapImageLayer
 			});
 		}
 		_layer.url = url;
-		jyzy.gis.arcgis._map.add(_layer);
+		gis.arcgis._map.add(_layer);
 		layer.mapLayer = _layer;
 		if (defaultResourceName == resourceName) {
-			jyzy.gis.arcgis._workLayer = layer;
+			gis.arcgis._workLayer = layer;
 		}
 	}
 	
@@ -211,33 +211,33 @@ jyzy.gis.arcgis.mapAddLayer = function(layers,baseUrl,FeatureLayer,MapImageLayer
  * geometry：几何区域，必须是extent类型
  * append:是否启用增量选择
  */
-jyzy.gis.arcgis.selectFeatureOnMap = function (geometry, append) {
+gis.arcgis.selectFeatureOnMap = function (geometry, append) {
 	var query = {
 		geometry : geometry,
 		spatialRelationship : 'intersects'
 	};
 	if(append != undefined && !append){
-		jyzy.gis.arcgis.clearSelectFeature();
+		gis.arcgis.clearSelectFeature();
 	}
-	jyzy.gis.arcgis._workLayer.view.queryFeatures(query).then(function(result){
+	gis.arcgis._workLayer.view.queryFeatures(query).then(function(result){
 		var features = result.features;
 		//选中要素执行函数
-		if(typeof(jyzy.gis.arcgis._mapClickEvent) == 'function'){
+		if(typeof(gis.arcgis._mapClickEvent) == 'function'){
 			let p = {
-				e : jyzy.gis.arcgis._clickEvent,
+				e : gis.arcgis._clickEvent,
 				features : features
 			}
-			jyzy.gis.arcgis._mapClickEvent(p);
+			gis.arcgis._mapClickEvent(p);
 		}
-		if(jyzy.gis.arcgis._selectable){
+		if(gis.arcgis._selectable){
 			for(let i = 0 ; i < features.length ; i++){
 				let fea = features[i];
-				let fid = fea.attributes[jyzy.gis.arcgis._workLayer.mapLayer.objectIdField];
-				if(jyzy.gis.arcgis._featureSelected(fid)){
-					jyzy.gis.arcgis.unSelectFeature(fid);
+				let fid = fea.attributes[gis.arcgis._workLayer.mapLayer.objectIdField];
+				if(gis.arcgis._featureSelected(fid)){
+					gis.arcgis.unSelectFeature(fid);
 				}
 				else{
-					jyzy.gis.arcgis.selectFeature(fea);
+					gis.arcgis.selectFeature(fea);
 				}
 			}
 		}
@@ -250,24 +250,24 @@ jyzy.gis.arcgis.selectFeatureOnMap = function (geometry, append) {
 /**
  * 取消图层选择要素
  */
-jyzy.gis.arcgis.clearSelectFeature = function(){
-	jyzy.gis.arcgis._mapView.graphics.removeAll();
-	jyzy.gis.arcgis._selectedFeatures = [];
+gis.arcgis.clearSelectFeature = function(){
+	gis.arcgis._mapView.graphics.removeAll();
+	gis.arcgis._selectedFeatures = [];
 	
 }
 /**
  * 设置是否启动选择
  */
-jyzy.gis.arcgis.setSelectable = function(selectable){
-	jyzy.gis.arcgis._selectable = selectable;
+gis.arcgis.setSelectable = function(selectable){
+	gis.arcgis._selectable = selectable;
 }
 
 /**
  * 判断要素是否被选中
  */
-jyzy.gis.arcgis._featureSelected = function(fid){
-	for(let i = 0 ; i < jyzy.gis.arcgis._selectedFeatures.length;i++){
-		let s = jyzy.gis.arcgis._selectedFeatures[i];
+gis.arcgis._featureSelected = function(fid){
+	for(let i = 0 ; i < gis.arcgis._selectedFeatures.length;i++){
+		let s = gis.arcgis._selectedFeatures[i];
 		if(s.fid == fid) return true;
 	}
 	return false;
@@ -275,10 +275,10 @@ jyzy.gis.arcgis._featureSelected = function(fid){
 /**
  * 根据要素ID，取消要素在图面上的选中状态
  */
-jyzy.gis.arcgis.unSelectFeature = function(fid){
+gis.arcgis.unSelectFeature = function(fid){
 	let gid;
 	let idx = -1;
-	for(let feature of jyzy.gis.arcgis._selectedFeatures){
+	for(let feature of gis.arcgis._selectedFeatures){
 		if(feature.fid == fid) {
 			idx = i;
 			gid = feature.gid;
@@ -286,10 +286,10 @@ jyzy.gis.arcgis.unSelectFeature = function(fid){
 		}
 	}
 	if(idx >= 0){
-		jyzy.gis.arcgis._selectedFeatures.splice(idx,1);
-		for(let item of jyzy.gis.arcgis._mapView.graphics.items.length){
+		gis.arcgis._selectedFeatures.splice(idx,1);
+		for(let item of gis.arcgis._mapView.graphics.items.length){
 			if(item.uid == gid){
-				jyzy.gis.arcgis._mapView.graphics.remove(item);
+				gis.arcgis._mapView.graphics.remove(item);
 				break;
 			}
 		}
@@ -299,16 +299,16 @@ jyzy.gis.arcgis.unSelectFeature = function(fid){
 /**
  * 根据要素设置要素被选中，添加到选中列表，并高亮显示
  */
-jyzy.gis.arcgis.selectFeature = function(feature){
-	let highlightGraphic = new jyzy.gis.arcgis._graphic(feature.geometry, jyzy.gis.arcgis._highlightSymbol);
+gis.arcgis.selectFeature = function(feature){
+	let highlightGraphic = new gis.arcgis._graphic(feature.geometry, gis.arcgis._highlightSymbol);
 	//添加一个高亮对象到地图
-	jyzy.gis.arcgis._mapView.graphics.add(highlightGraphic);
+	gis.arcgis._mapView.graphics.add(highlightGraphic);
 	
 	let s = {
-		fid : feature.attributes[jyzy.gis.arcgis._workLayer.mapLayer.objectIdField],
+		fid : feature.attributes[gis.arcgis._workLayer.mapLayer.objectIdField],
 		gid : highlightGraphic.uid
 	};
-	jyzy.gis.arcgis._selectedFeatures.push(s);
+	gis.arcgis._selectedFeatures.push(s);
 }
 
 /**
@@ -318,28 +318,28 @@ jyzy.gis.arcgis.selectFeature = function(feature){
  *    feature : 当前资源层中被选中的要素，为空代表没有任何选中
  * 
  */
-jyzy.gis.arcgis.setClickEvent = function(c){
-	jyzy.gis.arcgis._mapClickEvent = c;
+gis.arcgis.setClickEvent = function(c){
+	gis.arcgis._mapClickEvent = c;
 }
 
 
 /**
  * 设置当前操作资源图层
  */
-jyzy.gis.arcgis.changeResourceWorkLayer = function (resourceName) {
-	if(jyzy.gis.arcgis._workLayer.FEATURE_ID == resourceName) return;
+gis.arcgis.changeResourceWorkLayer = function (resourceName) {
+	if(gis.arcgis._workLayer.FEATURE_ID == resourceName) return;
 	
-	for (let layer of jyzy.gis.arcgis._layerList) {
+	for (let layer of gis.arcgis._layerList) {
         if (layer['FEATURE_ID'] == resourceName) {
-            jyzy.gis.arcgis._workLayer = layer;
+            gis.arcgis._workLayer = layer;
 			break;
         }
     }
 	
-	for (let item of jyzy.gis.arcgis._mapView.layerViews.items) {
+	for (let item of gis.arcgis._mapView.layerViews.items) {
 		let layer = item.layer;
-		if(layer.resourceName == jyzy.gis.arcgis._workLayer['FEATURE_ID']){
-			jyzy.gis.arcgis._workLayer.view = item;
+		if(layer.resourceName == gis.arcgis._workLayer['FEATURE_ID']){
+			gis.arcgis._workLayer.view = item;
 			break;
 		}
 	}
@@ -350,15 +350,15 @@ jyzy.gis.arcgis.changeResourceWorkLayer = function (resourceName) {
  * 根据元素ID定位元素：
  * ids:元素id列表，单个数字或者数字数组
  */
-jyzy.gis.arcgis.LocationFeatureByIds = function (fids,scale=2000) {
-	jyzy.gis.arcgis.clearSelectFeature();
+gis.arcgis.LocationFeatureByIds = function (fids,scale=2000) {
+	gis.arcgis.clearSelectFeature();
 	if(fids == '') return;
 	let fidList = fids;
 	
 	if(fidList instanceof Array){
 		fidList = fids.join();	
 	}
-	let layer = jyzy.gis.arcgis._workLayer.mapLayer;
+	let layer = gis.arcgis._workLayer.mapLayer;
 	let query = layer.createQuery();
 	//query.where = 'FID IN (' + fidList + ')';
 	query.objectIds = fidList;
@@ -367,37 +367,37 @@ jyzy.gis.arcgis.LocationFeatureByIds = function (fids,scale=2000) {
     
 	layer.queryFeatures(query).then(function(results) {
 		  let features = results.features;
-		  jyzy.gis.arcgis.mapViewGoTo({features:features,scale:scale});
+		  gis.arcgis.mapViewGoTo({features:features,scale:scale});
 		  for(let feature of features){
 			feature.attributes[layer.objectIdField];
-			jyzy.gis.arcgis.selectFeature(feature);
+			gis.arcgis.selectFeature(feature);
 		  }
 	 });
 	
 }
 
-jyzy.gis.arcgis._mapClickEvent = function(param){
+gis.arcgis._mapClickEvent = function(param){
 	if(param.scale == undefined){
 		param.scale=2000;
 	}
-	jyzy.gis.arcgis.mapViewGoTo(param);
+	gis.arcgis.mapViewGoTo(param);
 }
 /**
  * 根据选中要素移动图层
  */
-jyzy.gis.arcgis.mapViewGoTo = function(param){
+gis.arcgis.mapViewGoTo = function(param){
 	if(param == undefined || param == 'undefined') return;
 	var fs = param.features
 	if(fs.length == 0) return;
-	jyzy.gis.arcgis._mapView.goTo({target:fs,scale:param.scale});
+	gis.arcgis._mapView.goTo({target:fs,scale:param.scale});
 }
 
 /**
  * 获取当前图面上选中的要素ID列表
  */
-jyzy.gis.arcgis.getSelectedFeatureIds = function(){
+gis.arcgis.getSelectedFeatureIds = function(){
 	var ids = [];
-	for(let feature of jyzy.gis.arcgis._selectedFeatures){
+	for(let feature of gis.arcgis._selectedFeatures){
 		ids.push(feature.fid);
 	}
 	return ids;
@@ -407,7 +407,7 @@ jyzy.gis.arcgis.getSelectedFeatureIds = function(){
  * 选择要素查询该要素在其他图层位置
  * 默认查询楼栋图层（land）
  */
-jyzy.gis.arcgis.queryFeatureByFeature = function(feature,index=1){
+gis.arcgis.queryFeatureByFeature = function(feature,index=1){
 	let geoExtent = feature.geometry.extent;
 	let extent = {
 			xmin: geoExtent.xmin,
@@ -430,10 +430,10 @@ jyzy.gis.arcgis.queryFeatureByFeature = function(feature,index=1){
 	})
 }
 
-jyzy.gis.arcgis.locationResource = function(gucode,resourceName){
-	const layer = jyzy.gis.arcgis._workLayer.mapLayer;
+gis.arcgis.locationResource = function(gucode,resourceName){
+	const layer = gis.arcgis._workLayer.mapLayer;
 	if(layer.resourceName != resourceName){
-		jyzy.gis.arcgis.changeResourceWorkLayer(resourceName);
+		gis.arcgis.changeResourceWorkLayer(resourceName);
 	}
 	const url = curpath +'/jyzy/dataobject/data/dataname-jyzy_gis_feature_relation';
 	$.get(url,{resourceGucode:gucode,resourceName:resourceName},function(d){
@@ -443,6 +443,6 @@ jyzy.gis.arcgis.locationResource = function(gucode,resourceName){
 			fid += f.FID +',';
 		}
 		fid = fid.substring(0,fid.length-1)
-		jyzy.gis.arcgis.LocationFeatureByIds(fid);
+		gis.arcgis.LocationFeatureByIds(fid);
 	});
 }
